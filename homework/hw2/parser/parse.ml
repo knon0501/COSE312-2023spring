2f07d3a3d67151e59e30a121e3be5ad8b2b3d221
knon0501 <knon0501@daum.net>
2023-05-02 07:20:57 +0000

    hw2

--- homework/hw2/parser/parse.ml
+++ homework/hw2/parser/parse.ml
@@ -79,8 +79,264 @@ module ParsingTable = struct
   let for_all = BatMap.for_all
 end
 
-let check_LL1 : cfg -> bool
-=fun _ -> false (* TODO *)
+let rec getp: symbol->production list->symbol list list->symbol list list
+= fun s plst ret-> match plst with 
+                  | hd::tl -> (match hd with (x,y) -> 
+                    if x=s then (getp s tl (ret@[y])) else (getp s tl ret))
+                  | _-> ret;;
 
+
+
+
+let rec get_first: symbol ->production list -> FIRST.t -> symbol BatSet.t->symbol BatSet.t*bool
+= fun s plst first dup-> if (BatSet.mem s dup) then (BatSet.empty,true) else
+                      let dup = (BatSet.add s dup) in
+                      let exi= (FIRST.find s first) in
+                      if (BatSet.is_empty exi) then
+                      (let p = (getp s plst []) in
+                        list_fold (fun x (y1,y2) -> 
+    
+                          (
+                            if x=[] then (BatSet.add Epsilon y1,y2)
+                            else (
+                            let res=(
+                              list_fold (fun z (fst,flag,is_leftrec)-> 
+                              if flag then 
+                                  (
+                                  if z = s then (fst,flag,true) 
+                                  else ( 
+                                    let (fstz,flag2)=(get_first z plst first dup) in
+                                    let flag= ((BatSet.mem Epsilon fstz)&&flag) in
+                                    let fstz =(BatSet.remove Epsilon fstz) in                                
+                                    (BatSet.union fstz fst, flag,is_leftrec || flag2) 
+                                  )
+                                  )
+                              else (fst,flag,is_leftrec)
+                                ) x (y1,true,y2) 
+                                
+                              )
+                            in match res with (fst,flag,is_leftrec) -> 
+                              if is_leftrec then (BatSet.empty,true) else 
+                              (if flag then (BatSet.add Epsilon fst,is_leftrec) else (fst,is_leftrec))
+                            )
+                            ) 
+                            )
+                        p (BatSet.empty,false))
+                      else (exi,false);;
+
+let rec get_first2: symbol list -> FIRST.t -> symbol BatSet.t->symbol BatSet.t
+= fun lst first ret-> 
+                  match lst with hd::tl -> (let hdfst=(FIRST.find hd first) in
+                  let flag=(BatSet.mem Epsilon hdfst) in
+                  let hdfst=(BatSet.remove Epsilon hdfst) in
+                  if flag then (get_first2 tl first (BatSet.union hdfst ret)) else (BatSet.union hdfst ret))
+                  |_-> (BatSet.add Epsilon ret);;
+
+
+let rec is_nont : symbol ->symbol list->bool
+= fun x lst -> match lst with hd::tl -> if hd=x then true else (is_nont x tl) 
+                |_-> false;;
+
+let rec get_follow: symbol list->symbol list -> FOLLOW.t->FIRST.t->FOLLOW.t
+= fun nlst b follow first -> match b with hd::tl ->(
+              if is_nont hd nlst then 
+              (if tl=[] then follow else 
+                let fset= (BatSet.remove Epsilon (get_first2 tl first BatSet.empty)) in
+                let follow = (FOLLOW.add_set hd fset follow) in 
+                (get_follow nlst tl follow first))
+              else (get_follow nlst tl follow first)
+                )
+                |_-> follow;;
+let rec get_follow2: symbol->symbol list->symbol list -> FOLLOW.t->FIRST.t->FOLLOW.t
+= fun a nlst b follow first -> match b with hd::tl ->(
+              if is_nont hd nlst then 
+              (if tl=[] then follow else 
+                let fset= (get_first2 tl first BatSet.empty) in
+                if (BatSet.mem Epsilon fset) then 
+                let follow = (FOLLOW.add_set hd (FOLLOW.find a follow) follow) in 
+                (get_follow2 a nlst tl follow first)
+                else (get_follow2 a nlst tl follow first)
+                )
+              else (get_follow2 a nlst tl follow first)
+                )
+                |_-> follow;;
+let rec get_last: symbol list ->symbol
+= fun lst -> match lst with hd::tl -> (if tl=[] then hd else get_last tl)
+         |_->Epsilon;;
+let check_LL1 : cfg -> bool
+=fun g -> match g with (nlst,tlst,s,plst) ->  
+          let first= (list_fold (fun x y ->FIRST.add x x y) tlst FIRST.empty) in
+          let (first,is_leftrec)= (list_fold (fun x (fst,is_leftrec) -> 
+                      let (fstx,is_left) = (get_first x plst fst BatSet.empty) in
+                      let fst=(FIRST.add_set x fstx fst) in
+                      (fst,is_leftrec || is_left)
+            ) nlst (first,false)) in if is_leftrec then false 
+
+          else (
+            let follow = FOLLOW.empty in
+            let follow = (FOLLOW.add s End follow) in
+            let follow = 
+              (list_fold (fun p f -> match p with (_,lst)->
+                    (get_follow nlst lst f first)
+
+                ) plst follow) in
+            let follow = (list_fold (fun p f -> match p with (s,lst)->
+                    let b=(get_last lst) in
+                    if (is_nont b nlst) && b<>s then 
+                      let fols=(FOLLOW.find s f) in (FOLLOW.add_set b fols f) 
+                    else f
+                  ) 
+                  plst follow 
+              ) in
+              let follow = 
+                (list_fold (fun p f -> match p with (s,lst)->
+                      (get_follow2 s nlst lst f first)
+  
+                  ) plst follow) in
+                  let follow = 
+                    (list_fold (fun p f -> match p with (_,lst)->
+                          (get_follow nlst lst f first)
+      
+                      ) plst follow) in
+                  let follow = (list_fold (fun p f -> match p with (s,lst)->
+                          let b=(get_last lst) in
+                          if (is_nont b nlst) && b<>s then 
+                            let fols=(FOLLOW.find s f) in (FOLLOW.add_set b fols f) 
+                          else f
+                        ) 
+                        plst follow 
+                    ) in
+                    let follow = 
+                      (list_fold (fun p f -> match p with (s,lst)->
+                            (get_follow2 s nlst lst f first)
+        
+                        ) plst follow) in
+              let par= 
+                (list_fold (fun p par -> match p with (s,lst)->
+                      let fst = (BatSet.remove Epsilon (get_first2 lst first BatSet.empty)) in 
+                      (BatSet.fold
+                        (fun a y -> ParsingTable.add (s,a) p y)  fst par
+                      )
+                  )plst ParsingTable.empty ) in
+              let par= 
+                (list_fold (fun p par -> match p with (s,lst)->
+                      let fst =(get_first2 lst first BatSet.empty) in 
+                      if (BatSet.mem Epsilon fst) then 
+                      (BatSet.fold 
+                        (fun b y -> (ParsingTable.add (s,b) p y))
+                        (FOLLOW.find s follow) par
+                      ) else par
+                  )plst par ) in 
+              let res = (ParsingTable.foldi 
+                (fun _ pset flag -> 
+                    let cnt=(BatSet.cardinal pset) in
+                    (if cnt>=2 then false else flag)
+                )
+                par true 
+              ) in res
+          );;
+
+let get_LL1 : cfg -> ParsingTable.t
+=fun g -> match g with (nlst,tlst,s,plst) ->  
+          let first= (list_fold (fun x y ->FIRST.add x x y) tlst FIRST.empty) in
+          let (first,_)= (list_fold (fun x (fst,is_leftrec) -> 
+                      let (fstx,is_left) = (get_first x plst fst BatSet.empty) in
+                      let fst=(FIRST.add_set x fstx fst) in
+                      (fst,is_leftrec || is_left)
+            ) nlst (first,false)) in 
+            let (first,_)= (list_fold (fun x (fst,is_leftrec) -> 
+              let (fstx,is_left) = (get_first x plst fst BatSet.empty) in
+              let fst=(FIRST.add_set x fstx fst) in
+              (fst,is_leftrec || is_left)
+           ) nlst (first,false)) in 
+    
+           (
+            let follow = FOLLOW.empty in
+            let follow = (FOLLOW.add s End follow) in
+            let follow = 
+              (list_fold (fun p f -> match p with (_,lst)->
+                    (get_follow nlst lst f first)
+
+                ) plst follow) in
+            let follow = (list_fold (fun p f -> match p with (s,lst)->
+                    let b=(get_last lst) in
+                    if (is_nont b nlst) && b<>s then 
+                      let fols=(FOLLOW.find s f) in (FOLLOW.add_set b fols f) 
+                    else f
+                  ) 
+                  plst follow 
+              ) in
+              let follow = 
+ 
+                (list_fold (fun p f -> match p with (s,lst)->
+                 
+                (get_follow2 s nlst lst f first)
+                 
+                 
+                ) plst follow) 
+             in
+             let follow = 
+              (list_fold (fun p f -> match p with (_,lst)->
+                    (get_follow nlst lst f first)
+
+                ) plst follow) in
+            let follow = (list_fold (fun p f -> match p with (s,lst)->
+                    let b=(get_last lst) in
+                    if (is_nont b nlst) && b<>s then 
+                      let fols=(FOLLOW.find s f) in (FOLLOW.add_set b fols f) 
+                    else f
+                  ) 
+                  plst follow 
+              ) in
+              let follow = 
+ 
+                (list_fold (fun p f -> match p with (s,lst)->
+                 
+                (get_follow2 s nlst lst f first)
+                 
+                 
+                ) plst follow) 
+             in
+              let par= 
+                (list_fold (fun p par -> match p with (s,lst)->
+                      let fst = (BatSet.remove Epsilon (get_first2 lst first BatSet.empty)) in 
+                      (BatSet.fold
+                        (fun a y -> ParsingTable.add (s,a) p y)  fst par
+                      )
+                  )plst ParsingTable.empty ) in
+              let par= 
+                (list_fold (fun p par -> match p with (s,lst)->
+                      let fst =(get_first2 lst first BatSet.empty) in 
+                      if (BatSet.mem Epsilon fst) then 
+                      (BatSet.fold 
+                        (fun b y -> (ParsingTable.add (s,b) p y))
+                        (FOLLOW.find s follow) par
+                      ) else par
+                  )plst par ) 
+              
+              in par
+          );;
+
+let rec parse2 : ParsingTable.t -> symbol list ->symbol list -> bool
+= fun table input stack -> 
+    
+    match input with a::itl ->(
+    match stack with x::stl ->
+
+    if x=End then (a=End) else 
+      (
+        if x=a then (parse2 table itl stl) else
+        let p=(ParsingTable.find (x,a) table) in
+        if (BatSet.is_empty p) then false else(
+          let (_,y) =BatSet.choose p in
+       
+              (parse2 table input (y@stl)))
+            )
+    |_-> true)
+    |_->true;;
 let parse : cfg -> symbol list -> bool
-=fun _ _ -> false (* TODO *)
+=fun cfg lst -> match cfg with (_,_,s,_)->
+
+  let table = (get_LL1 cfg) in 
+
+  (parse2 table (lst@[End]) (s::[End]));;
--- homework/hw2/regex2dfa/trans.ml
+++ homework/hw2/regex2dfa/trans.ml
@@ -2,18 +2,116 @@ open Regex
 
 exception Not_implemented
 
-let regex2nfa : Regex.t -> Nfa.t 
-=fun _ -> raise Not_implemented (* TODO *)
+let get_final : Nfa.t-> Nfa.state
+= fun t -> let fins=(Nfa.get_final_states t) in
+                (BatSet.choose fins);;
+let rec regex2nfa : Regex.t -> Nfa.t 
+=fun t -> match t with
+  | Empty -> let nfa=Nfa.create_new_nfa() in
+            let (x,nfa)=(Nfa.create_state nfa) in
+            (Nfa.add_final_state nfa x) 
+  | Epsilon -> let nfa=Nfa.create_new_nfa() in
+              let (x,nfa)=(Nfa.create_state nfa) in
+              let nfa=(Nfa.add_final_state nfa x) in
+              let init=(Nfa.get_initial_state nfa) in 
+              (Nfa.add_epsilon_edge nfa (init,x))
+  | Alpha(c) -> let nfa=Nfa.create_new_nfa() in
+              let (x,nfa)=(Nfa.create_state nfa) in
+              let nfa=(Nfa.add_final_state nfa x) in
+              let init=(Nfa.get_initial_state nfa) in 
+              (Nfa.add_edge nfa (init,c,x))
+  | OR(r1,r2) -> let n1=(regex2nfa r1) in
+                 let n2=(regex2nfa r2) in
+                 let n3=Nfa.create_new_nfa() in
+                let (fin,n3)=(Nfa.create_state n3) in
+                let n3=(Nfa.add_final_state n3 fin) in
+                let init=(Nfa.get_initial_state n3) in
+                let n3=(Nfa.add_states n3 (Nfa.get_states n1)) in
+                let n3=(Nfa.add_states n3 (Nfa.get_states n2)) in
+                let n3=(Nfa.add_edges n3 (Nfa.get_edges n1)) in
+                let n3=(Nfa.add_edges n3 (Nfa.get_edges n2)) in
+                let n3=(Nfa.add_epsilon_edge n3 (init, (Nfa.get_initial_state n1))) in
+                let n3=(Nfa.add_epsilon_edge n3 (init, (Nfa.get_initial_state n2))) in
+                let n1_f = get_final(n1) in
+                let n2_f = get_final(n2) in
+                let n3=(Nfa.add_epsilon_edge n3 (n1_f, fin)) in  
+                let n3=(Nfa.add_epsilon_edge n3 (n2_f, fin)) in  n3
+  | CONCAT(r1,r2) -> let n1=(regex2nfa r1) in
+                      let n2=(regex2nfa r2) in
+                      let n3=Nfa.create_new_nfa() in
+                    let init=(Nfa.get_initial_state n3) in
+                    let n3=(Nfa.add_states n3 (Nfa.get_states n1)) in
+                    let n3=(Nfa.add_states n3 (Nfa.get_states n2)) in
+                    let n3=(Nfa.add_edges n3 (Nfa.get_edges n1)) in
+                    let n3=(Nfa.add_edges n3 (Nfa.get_edges n2)) in
+                    let n3=(Nfa.add_epsilon_edge n3 (init, (Nfa.get_initial_state n1))) in
+                    let n3=(Nfa.add_epsilon_edge n3 (get_final n1, (Nfa.get_initial_state n2))) in
+                    let n3=(Nfa.add_final_state n3 (get_final n2)) in n3
+  | STAR(r) ->let n1=(regex2nfa r) in
+              let n2=Nfa.create_new_nfa() in
+              let (x,n2)=Nfa.create_state(n2) in
+              let n2=(Nfa.add_final_state n2 x) in
+              let n2=(Nfa.add_states n2 (Nfa.get_states n1)) in
+              let n2=(Nfa.add_edges n2 (Nfa.get_edges n1)) in
+              let n2=(Nfa.add_epsilon_edge n2 (Nfa.get_initial_state n2,Nfa.get_initial_state n1)) in
+              let n2=(Nfa.add_epsilon_edge n2 (get_final n1,get_final n2)) in
+              let n2=(Nfa.add_epsilon_edge n2 (Nfa.get_initial_state n2,get_final n2)) in 
+              let n2=(Nfa.add_epsilon_edge n2 (get_final n1,Nfa.get_initial_state n1)) in n2;;
+
+let is_final: Dfa.state -> Nfa.t-> bool
+= fun state nfa-> BatSet.fold (fun x y -> (Nfa.is_final_state nfa x) || y) state false;;
+
+
+let rec epsclosure: Nfa.t -> Nfa.state BatSet.t-> Nfa.state BatSet.t->Dfa.state
+= fun nfa s states -> if (BatSet.is_empty states) then s else
+                      let t= (BatSet.fold
+                      (fun x y -> (BatSet.union (Nfa.get_next_state_epsilon nfa x) y ))
+                      states BatSet.empty
+                      ) in (epsclosure nfa (BatSet.union s t) (BatSet.diff t s));;
+
+let dfa_next: Nfa.t->Dfa.state->alphabet->Dfa.state
+= fun nfa state c  -> (BatSet.fold
+                      (fun x y -> (BatSet.union (Nfa.get_next_state nfa x c) y))
+                        state BatSet.empty
+                      );;
+
+let rec nfa3dfa : (Dfa.state BatSet.t)->(Dfa.state list)->(Nfa.t)->(Dfa.t)->(Dfa.t) 
+= fun d w  nfa dfa -> match w with 
+                      | hd::tl -> let astate= (dfa_next nfa hd A) in
+                        let astate = epsclosure nfa astate astate in
+                        let bstate = (dfa_next nfa hd B) in
+                        let bstate = epsclosure nfa bstate bstate in
+                        let w = (if (BatSet.mem astate d) then tl else tl@[astate]) in
+                        let w = (if (BatSet.mem bstate d) then w else w@[bstate]) in
+                        let d=(BatSet.add astate (BatSet.add bstate d)) in
+                        let dfa=(Dfa.add_state dfa astate) in
+                        let dfa=(Dfa.add_state dfa bstate) in
+                        let dfa=(Dfa.add_edge dfa (hd,A,astate)) in
+                        let dfa=(Dfa.add_edge dfa (hd,B,bstate)) in
+                        let dfa=(if is_final astate nfa then (Dfa.add_final_state dfa astate) else dfa) in
+                        let dfa=(if is_final bstate nfa then (Dfa.add_final_state dfa bstate) else dfa) in
+                        (nfa3dfa d w nfa dfa )
+                      |  _->dfa;;
 
 let nfa2dfa : Nfa.t -> Dfa.t
-=fun _ -> raise Not_implemented (* TODO *)
- 
-(* Do not modify this function *)
+=fun t -> let s = (Nfa.get_initial_state t) in
+          let initset = (BatSet.singleton s) in
+          let _s= (epsclosure t initset initset ) in
+          let dfa = (Dfa.create_new_dfa _s) in
+          let dfa = if (is_final _s t) then Dfa.add_final_state dfa _s  else dfa in
+          let w= [_s] in (nfa3dfa (BatSet.singleton _s) w t dfa);;
+
+          (* Do not modify this function *)
 let regex2dfa : Regex.t -> Dfa.t
 =fun regex -> 
   let nfa = regex2nfa regex in
   let dfa = nfa2dfa nfa in
-    dfa
+    dfa;;
+
+let rec run_dfa2 : Dfa.t -> Dfa.state -> alphabet list -> bool
+= fun dfa state lst -> match lst with
+                    | hd::tl -> run_dfa2 dfa (Dfa.get_next_state dfa state hd) tl;
+                    | _ -> (Dfa.is_final_state dfa state);;
 
 let run_dfa : Dfa.t -> alphabet list -> bool
-=fun _ _ -> raise Not_implemented (* TODO *)
+=fun dfa lst -> run_dfa2 dfa (Dfa.get_initial_state dfa) lst;;
